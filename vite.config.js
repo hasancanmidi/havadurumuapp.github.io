431d9b8c6523341b253df74143e7717f34df2d71
mac <Hasancanmidi@hotmail.com>
2024-02-21 05:06:42 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/havadurumuapp.github.io/',
+  base: '/havadurumuapp/',
   plugins: [react()]
 });
